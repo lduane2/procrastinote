229cdc1930fcbaa9fd3f32ae6bed6391bfd15325
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 10:13:10 -0400

    add html and edit urls/views

--- mysite/noted/urls.py
+++ mysite/noted/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<file_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^upload/$', views.upload_file, name='upload'),
-    url(r'^uploaded/(?P<file_id>[0-9]+)/$', views.uploaded, name='upload_file'),
+    url(r'^uploaded/(?P<file_id>[0-9]+)/$', views.uploaded, name='uploaded'),
 ]
--- mysite/noted/views.py
+++ mysite/noted/views.py
@@ -25,7 +25,7 @@ def upload_file(request):
 		form = UploadFileForm(request.POST, request.FILES)
 		if form.is_valid():
 			handle_uploaded_file(request.FILES['file'])
-			return HttpResponseRedirect('noted/uploaded/')
+			return HttpResponseRedirect('noted/detail/')
 		else:
 			form = UploadFileForm()
 		return render(request, 'upload.html', {'form': form})
