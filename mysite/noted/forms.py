e6cbc18d1a5ef8cd06eb7a35df529e76faab8586
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 11:37:24 -0400

    add upload file capability

--- mysite/noted/forms.py
+++ mysite/noted/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 
 class ModelFormWithFileField(forms.Form):
     title = forms.CharField(max_length=50)
--- mysite/noted/views.py
+++ mysite/noted/views.py
@@ -4,8 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .forms import ModelFormWithFileField
+from django.utils import timezone
 
+from .forms import ModelFormWithFileField
 from .models import UploadedFile
 
 def index(request):
@@ -20,18 +21,12 @@ def detail(request,upload_id):
     return render(request, 'noted/detail.html', {'upload': uf})
 
 def upload_file(request):
-	if request.method == 'POST':
-		form = ModelFormWithFileField(request.POST, request.FILES)
-		try:
-            newupload = UploadedFile(
-                file_name=request.POST['filename'],
-                upload_date=timezone.now(),
-                file_contents=request.POST['uploadedfile']
-            )
-			newupload.save()
-			#handle_uploaded_file(request.FILES['file'])
-			#my_model.save()
-			return HttpResponseRedirect('noted/')
-		except:
-            form = ModelFormWithFileField()
-		return render(request, 'upload.html', {'form': form})
+    if request.method == 'POST':
+        #form = ModelFormWithFileField(request.POST, request.FILES)
+        newupload = UploadedFile(file_name=request.POST['filename'],upload_date=timezone.now(),file_contents=request.POST['uploadedfile'])
+        newupload.save()
+        #handle_uploaded_file(request.FILES['file'])
+        #my_model.save()
+        return HttpResponseRedirect('noted/')
+
+        return render(request, 'upload.html')
