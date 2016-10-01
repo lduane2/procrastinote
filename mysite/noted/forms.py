e1b674d9934d1dcfd1d521ac0ed8043f574dee40
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 10:51:39 -0400

    prepare for merge

--- mysite/noted/forms.py
+++ mysite/noted/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class ModelFormWithFileField(forms.Form):
     title = forms.CharField(max_length=50)
-    file = forms.FileField()
+    upload = forms.FileField()
     #data above accessible by request.FILES['file']
 
