b5205209142527f07a254164fafd180924017065
lduane2 <lukeduane0@gmail.com>
2016-10-01 09:30:24 -0400

    Trying to set up handling an uploaded file

--- mysite/noted/urls.py
+++ mysite/noted/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'noted'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<file_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^uploaded/($P<file_id>[0-9]+/$', views.uploaded, name='uploaded'),
 ]
--- mysite/noted/views.py
+++ mysite/noted/views.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .forms import UploadFileForm
 
 from .models import UploadedFile
+
 
 def index(request):
     latest_uploads_list = UploadedFile.objects.order_by('-upload_date')[:5]
@@ -17,3 +19,19 @@ def index(request):
 def detail(request,upload_id):
     f = get_object_or_404(File, pk=upload_id)
     return render(request, 'noted/detail.html', {'upload': upload})
+
+def upload_file(request):
+	if request.method == 'POST':
+		form = UploadFileForm(request.POST, request.FILES)
+		if form.is_valid():
+			handle_uploaded_file(request.FILES['file'])
+			return HttpResponseRedirect('noted/uploaded/')
+		else:
+			form = UploadFileForm()
+		return render(request, 'upload.html', {'form': form})
+
+def handle_uploaded_file(f):
+	with open('./uploadedFiles/tmp', 'wb+') as destination:
+		for chunk in f.chunks():
+			destination.write(chunk)
+
