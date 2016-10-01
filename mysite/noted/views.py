5213794d2d2f85935f40f8fd05a48576258a97dd
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 12:01:52 -0400

    undo changes to views

--- mysite/noted/views.py
+++ mysite/noted/views.py
@@ -4,9 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from django.utils import timezone
-
 from .forms import ModelFormWithFileField
+
 from .models import UploadedFile
 
 def index(request):
@@ -21,12 +20,12 @@ def detail(request,upload_id):
     return render(request, 'noted/detail.html', {'upload': uf})
 
 def upload_file(request):
-    if request.method == 'POST':
-        #form = ModelFormWithFileField(request.POST, request.FILES)
-        newupload = UploadedFile(file_name=request.POST['filename'],upload_date=timezone.now(),file_contents=request.POST['uploadedfile'])
-        newupload.save()
-        #handle_uploaded_file(request.FILES['file'])
-        #my_model.save()
-        return HttpResponseRedirect('noted/')
-
-        return render(request, 'upload.html')
+	if request.method == 'POST':
+		form = ModelFormWithFileField(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			#handle_uploaded_file(request.FILES['file'])
+			return HttpResponseRedirect('noted/')
+		else:
+			form = ModelFormWithFileField()
+		return render(request, 'upload.html', {'form': form})
