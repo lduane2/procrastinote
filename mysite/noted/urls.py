423a4bca4ec5249e48a3cf187b8946bbaf04ba56
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 16:21:19 -0400

    start website design

--- mysite/noted/urls.py
+++ mysite/noted/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'noted'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^list/$', views.list, name='list'),
     url(r'^(?P<upload_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^upload/$', views.upload_file, name='upload'),
     #url(r'^uploaded/(?P<file_id>[0-9]+)/$', views.uploaded, name='upload_file'),
