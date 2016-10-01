400ca65554b3206c53dc1a1af6762e02a695401a
akeenan2 <ann.keenan2@gmail.com>
2016-10-01 16:59:44 -0400

    forms

--- mysite/noted/forms.py
+++ mysite/noted/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.utils import timezone
 
 class ModelForm(forms.Form):
-    file_name = forms.CharField(max_length=200)
-    upload_date = forms.DateTimeField(timezone.now())
+    file_name = forms.CharField(widget=forms.TextInput(attrs={'class' : 'myfieldclass'}))
     file_contents = forms.FileField()
     #data above accessible by request.FILES['file']
--- mysite/noted/views.py
+++ mysite/noted/views.py
@@ -4,9 +4,21 @@ from __future__ import unicode_literals
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .forms import ModelForm
 
+import re
+import json
+import os
+from os.path import join, dirname
+from watson_developer_cloud import DocumentConversionV1
+
+from .forms import ModelForm
 from .models import UploadedFile
+
+
+document_conversion = DocumentConversionV1(
+                                           username='632d20d2-21d6-4b72-b1e4-35409db12fe3',
+                                           password='nB2m1r40gUIi',
+                                           version='2016-02-09')
 
 def index(request):
     uploads_list = UploadedFile.objects.order_by('-upload_date')
@@ -15,10 +27,6 @@ def index(request):
             }
     return render(request, 'noted/index.html', context)
 
-def detail(request,upload_id):
-    upload = get_object_or_404(UploadedFile, pk=upload_id)
-    return render(request, 'noted/detail.html', {'upload': upload})
-
 def list(request):
     uploads_list = UploadedFile.objects.order_by('-upload_date')
     context = {
@@ -26,16 +34,25 @@ def list(request):
             }
     return render(request, 'noted/list.html', context)
 
+def detail(request,upload_id):
+    os.system('pwd')
+    upload = get_object_or_404(UploadedFile, pk=upload_id)
+    with open('noted/uploads/Fall_2016_Resume.pdf', 'r') as document:
+        config = {'conversion_target': DocumentConversionV1.NORMALIZED_HTML}
+        response = document_conversion.convert_document(document=document, config=config).content
+        m = re.search('<body>(.*)</body>', response)
+        found = m.group(1)
+        print(found)
+    return render(request, 'noted/detail.html', {'found': found, 'upload': upload} )
+
 def upload_file(request):
     if request.method == 'POST':
         form = ModelForm(request.POST)
         if form.is_valid():
-            try:
-                form.save()
-                #handle_uploaded_file(request.FILES['file'])
-                return HttpResponseRedirect('noted/')
-            except:
-                return HttpResponseRedirect('upload/')
+            form.upload_date = timezone.now()
+            print form['file_name']
+            form.save()
+            return HttpResponseRedirect('noted/list/')
     else:
         form = ModelForm()
     return render(request, 'noted/upload.html', {'form': form})
