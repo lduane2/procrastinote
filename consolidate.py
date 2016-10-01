739655ca1f14013c699fc5067a09735659379d2f
John Bruscia <jbruscia@nd.edu>
2016-10-01 15:22:00 -0400

    paragraph stuff

--- consolidate.py
+++ consolidate.py
@@ -1,13 +1,25 @@
 # coding=utf-8
 from os.path import join, dirname
 import string
-
-keyWord = "orange"
+import sys
+keyWord = "nonphysical"
+pmode = 1
 f1 = open(join(dirname(__file__), './consolidate.txt'), 'w+')
-with open(join(dirname(__file__), './rawText.txt'), 'r') as document:
-    for lines in document:
-        if (lines.find(keyWord) != -1):
-            f1.write(lines)
+with open(join(dirname(__file__), './dualism.txt'), 'r') as document:
+    if (pmode):
+    	currentp = ""
+    	for lines in document:
+			if(len(lines) == 0):
+				if(currentp.find(keyWord) != 1):
+					f1.write(currentp)
+				
+			else:
+				currentp += lines
+			currentp = ""
+	else:	
+		for lines in document:
+		    if (lines.find(keyWord) != -1):
+		        f1.write(lilsnes)
 
 
 f1.close()
