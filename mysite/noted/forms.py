c74946d2913256e05fb36fe15f720b97e058b389
akeenan2 <ann.keenan2@gmail.com>
2016-10-02 02:11:12 -0400

    fix merge conflict

--- mysite/noted/forms.py
+++ mysite/noted/forms.py
@@ -9,12 +9,4 @@ class UploadFileForm(forms.ModelForm):
         exclude = ['upload_date','file_path','folder']
     file_name = forms.CharField(max_length=200)
     file_contents = forms.FileField()
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-#class Form(forms.Form):
-   # haha = 'lol'
-=======
->>>>>>> cd24d4dad7c9b824ec5114c122b23516598a7713
->>>>>>> 4f715c3e45c3a23dbd58aaa73f35035aeebae93d
+    
